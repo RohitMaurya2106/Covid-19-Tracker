eb7e52e95109e8b3d7cf8df5d319fa96b8a4ba50
Rohit_Maurya2106 <16.ics.047.gbu@gmail.com>
2020-08-05 18:10:29 +0530

    Stats graph' Covid-19

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from 'react';
-import {MenuItem, FormControl,Select} from "@material-ui/core";
+import {MenuItem, FormControl,Select, Card, CardContent} from "@material-ui/core";
+import InfoBox from "./InfoBox";
 import './App.css';
+import Map from "./Map";
+import Table from "./Table";
+import { sortData } from './util';
+import LineGraph from "./LineGraph";
 
 function App() {
 
   const[countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then((response)=> response.json())
+    .then((data)=>{
+      setCountryInfo(data);
+    });
+  },[]);
+
   useEffect(() =>{
       const getCountriesData = async() =>{
         await fetch("https://disease.sh/v3/covid-19/countries")
@@ -13,22 +29,35 @@ function App() {
         .then((data)=>{
           const countries = data.map((country)=>({
              name: country.country,
-             value: country.countryInfo.iso2
+             value: country.countryInfo.iso2,
           }
           ));
+          const sortedData = sortData(data);
+            setTableData(sortedData);
             setCountries(countries);
         });
       };
       getCountriesData();
   },[]); 
 
-  const onCountryChange = (event)=>{
+  const onCountryChange = async (event)=>{
     const countryCode = event.target.value;
     setCountry(countryCode);
+
+    const url= countryCode=== "worldwide" ? "https://disease.sh/v3/covid-19/all" : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+    .then((response)=>response.json())
+    .then((data)=>{
+      setCountry(countryCode);
+      setCountryInfo(data);
+    });
+    //https://disease.sh/v3/covid-19/countries/[COUNTRY_CODE]
+    //"https://disease.sh/v3/covid-19/all"
   }; 
 
   return (
     <div className="app">
+      <div className="app_left">
       <div className="app_header">
         <h1>COVID 19 TRACKER</h1>
         <FormControl className="app_dropdown">
@@ -39,22 +68,31 @@ function App() {
               <MenuItem value={country.value}>{country.name}</MenuItem>
               ))
             }
-            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option 2</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem> */}
           </Select>
         </FormControl>
       </div>
       
-      {/* Header */}
-      {/* Title + Select input dropdown field */}
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-      {/* Table */}
-      {/* Graph */}
-      {/* Map */}
+      <div className="app_stats">
+            <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+            <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+            <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+      </div>
+      
+      
+          {/* Map */}
+          <Map />
+      </div>
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+            {/* Table */}
+            <Table countries={tableData}></Table>
+            <h3>Worldwide new cases</h3>
+            <LineGraph />
+            {/* Graph */}
+        </CardContent>
+            
+      </Card>
     </div>
   );
 }
